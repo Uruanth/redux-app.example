b30239fa08be8e6cde267c35dcc0ebf80dd35f9c
Dairon-Perilla <100379630+Dairon-Perilla@users.noreply.github.com>
2022-12-13 20:13:34 -0500

    firts session

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { decrement, increment, reset } from './lib/redux/actions/acciones.action';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'redux-app';
+  count$: Observable<number>
+ 
+  constructor(private store: Store<{ contadores: number }>) {
+    this.count$
+     = store.select('contadores');
+  }  
+ 
+  increment() {
+    this.store.dispatch(increment())
+  }
+  
+  decrement() {
+    this.store.dispatch(decrement())
+  }
+  
+  reset() {
+    this.store.dispatch(reset())
+  }
+
+  ngOnDestroy(): void {
+  }
+
 }
---
+++ src/app/lib/redux/actions/acciones.action.ts
@@ -0,0 +1,8 @@
+import { createAction } from '@ngrx/store';
+
+export const increment = createAction('[Counter Component] Increment');
+export const decrement = createAction('[Counter Component] Decrement');
+export const reset = createAction('[Counter Component] Reset');
+export const login = createAction('[Login] logear usuario');
+export const loginExito = createAction('[Login] logear usuario exito');
+export const loginError = createAction('[Login] logear usuario error');
