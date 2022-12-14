d26f65c682f50fa42b6f9a5e32e8b9edd2602f80
Dairon-Perilla <100379630+Dairon-Perilla@users.noreply.github.com>
2022-12-14 08:13:26 -0500

    test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,49 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { MockStore, provideMockStore } from '@ngrx/store/testing'
+import { NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Store } from '@ngrx/store';
 
+const estadoInicial: { contadores: number, prueba: number } = {
+  contadores: 0,
+  prueba: 0
+}
 describe('AppComponent', () => {
+
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let store: MockStore<{ contadores: number, prueba: number }>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
       ],
+      schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
       declarations: [
         AppComponent
       ],
+      providers: [
+        provideMockStore({ initialState: estadoInicial }),
+
+      ],
     }).compileComponents();
-  });
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'redux-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('redux-app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    store = TestBed.inject(MockStore)
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('redux-app app is running!');
   });
+
+
+  it('should create', () => {
+    store.subscribe(console.log)
+    store.setState({
+      contadores: 11,
+      prueba: 8
+    })
+    store.subscribe(console.log)
+    expect(component).toBeTruthy();
+  });
+
 });
